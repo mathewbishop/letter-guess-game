85eae7426d52eda11505bba5817de12e117066d1
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-08-20 18:37:35 -0500

    added text vars, working on guesses remaining counter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,19 +15,36 @@
 var wins = 0;
 var losses = 0;
 
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var guessesRemainingText = document.getElementById("guessesRemaining-text");
+var guessedLettersText = document.getElementById("guessedLetters-text");
+var test = document.getElementById("test-text");
 
 
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+
+    
+
 document.onkeyup = function(event) {
     var userGuess = event.key;    
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-    if (userGuess === computerGuess) {
-        wins++;
-    }
-    else {
-        losses++;
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)]; 
+    var guessesRemaining = 7;
+    for (var i = 0; i>0; i--) {
+        guessesRemaining[i];
     }
 
-}
+        if (userGuess === computerGuess) {
+            wins++;
+        }
+        else {
+            losses++;
+        }
+    
+
+    winsText.textContent = "Breakthroughs: " + wins;
+    lossesText.textContent = "Failures: " + losses;
+    guessesRemainingText.textContent = "Guesses Remaining " + guessesRemaining;
+  
+};
