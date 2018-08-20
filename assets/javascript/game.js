0f2080ef30f3699fc79eac99566e9812c487e64a
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-08-20 14:29:48 -0500

    restarted project, will update repo name

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,16 @@
+// letter guess game
+// computer guesses a letter, and the user must guess the correct letter to win
+// components
+    // list of computer choices
+    // computer picks a random letter
+    // record user input to get the user's guess
+    // compare the user guess with the computer guess to determine a winner
+        // counters
+            // wins
+            // losses
+            // number of remaining guesses
+            // guesses so far (display letters
+    // on win or lose, restart game without page refresh
 
-// display words/placeholder
-// user input to guess letters          keypress event
-// limited number of letter guesses     if statements?
-// guesses remaining recorder           if statement/loop?
-// placeholder for the word user is trying to guess(underscores)
-// determine whether guess was correct or incorrect         if statement
-// record wins                                          
-// record losses
-// game over and restart
-
-
-var wordList = ["Grey Hat", "Back Door", "Brute Force Attack", "Doxing", "Keylogger"];
-var wordSetText = document.getElementById("wordSet-text"); 
-var wrongGuessText = document.getElementById("wrongGuess-text");
-var wordChoice = wordList[Math.floor(Math.random() * wordSet.length)];
-
-
-document.onkeypress = function(event) {
-    var userGuess = event.key;
-
-    if (userGuess === wordChoice[]) {
-        
-    }   
-
-
-
-}
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
