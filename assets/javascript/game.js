c1643811ed86f156fbbde46117e08d4ddcfac98b
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-11-26 15:58:13 -0600

    word data defined

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,7 @@
 // DOM Nodes
 //===================================================================
 const hiddenWord = document.getElementById("hidden-word");
+const hint = document.getElementById("hint");
 const clock = document.getElementById("clock");
 const guessed = document.getElementById("guessed-letters");
 const usrInput = document.getElementById("user-input");
@@ -9,5 +10,27 @@ const usrInput = document.getElementById("user-input");
 // Generate Hidden Word
 //===================================================================
 function wordGen() {
-    let wordBank = ["The", "Grasshopper", "Lies", "Heavy"]
+    let wordBank = [
+        {
+            word: "Alpha",
+            hint: "Greek for 'first'."
+        },
+        {
+            word: "Propaganda",
+            hint: "The vehicle that carries the 'big lie' to the masses."
+        },
+        {
+            word: "Darknet",
+            hint: "The internet, with the lights turned off."
+        },
+        {
+            word: "Corporatocracy",
+            hint: "Rule by the corporations."
+        },
+        {
+            word: "Neon",
+            hint: "The light that bathes modern cities."
+        }
+    ]
+    return Math.floor(Math.random() * wordBank.length)
 }
