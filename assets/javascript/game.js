1a21b071b079a1c44a87416502e39a619e994b73
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-11-25 23:45:23 -0600

    start wordGen function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,3 +5,9 @@ const hiddenWord = document.getElementById("hidden-word");
 const clock = document.getElementById("clock");
 const guessed = document.getElementById("guessed-letters");
 const usrInput = document.getElementById("user-input");
+//===================================================================
+// Generate Hidden Word
+//===================================================================
+function wordGen() {
+    let wordBank = ["The", "Grasshopper", "Lies", "Heavy"]
+}
