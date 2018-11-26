77e971dcc08c20e0c9e93d395b69d046480fb4fc
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-11-25 23:26:16 -0600

    add JS DOM node vars

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+//===================================================================
+// DOM Nodes
+//===================================================================
+const hiddenWord = document.getElementById("hidden-word");
+const clock = document.getElementById("clock");
+const guessed = document.getElementById("guessed-letters");
+const usrInput = document.getElementById("user-input");
